f81b9dd1af42bc321f742dcc0527f855e5a0cc5a
Artem-WebDeveloper <ave_art@icloud.com>
2026-07-18 20:50:39 +0300

    feat: add database

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-// console.log(process.env);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose.connect(DB).then((con) => {
+  console.log(con.connections);
+});
+
+/* mongoose.connect(process.env.DATABASE_LOCAL).then(async (con) => {
+  // console.log(con.connections);
+  const collections = await con.connection.db.listCollections().toArray();
+  console.log(collections.map((c) => c.name));
+}); */
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
