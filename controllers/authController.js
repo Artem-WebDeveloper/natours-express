24adc74ee96ab076fb5f5c12a8f104af6d32d2cd
Artem-WebDeveloper <ave_art@icloud.com>
2026-08-17 18:19:50 +0300

    feat: implement update user's password functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,6 +12,16 @@ const signToken = (userId) =>
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: { user },
+  });
+};
+
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
@@ -21,13 +31,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     role: req.body.role,
   });
 
-  const token = signToken(newUser._id);
-
-  res.status(201).json({
-    status: 'success',
-    token,
-    data: { user: newUser },
-  });
+  createSendToken(newUser, 201, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -47,12 +51,7 @@ exports.login = catchAsync(async (req, res, next) => {
 
   // 3. Отправить token, если все ок
 
-  const token = signToken(user._id);
-
-  res.status(200).json({
-    status: 'success',
-    token,
-  });
+  createSendToken(user, 200, res);
 });
 
 exports.protect = catchAsync(async (req, res, next) => {
@@ -175,10 +174,25 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
 
   // 4. Залогировать юзера, отправить JWT
 
-  const token = signToken(user._id);
+  createSendToken(user, 200, res);
+});
 
-  res.status(200).json({
-    status: 'success',
-    token,
-  });
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  // 1. Получить пользователя из коллекции
+  const user = await User.findById(req.user.id).select('+password');
+
+  // 2. Проверить корректность введенного пароля
+  if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
+    return next(new AppError('Your current password is wrong.', 401));
+  }
+
+  // 3. Если пароль верный - обновить
+
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save();
+  // User.findByIdAndUpdate - не сработало бы с проверками модели User
+
+  // 4. Авторизовать пользователя, отправить JWT
+  createSendToken(user, 200, res);
 });
