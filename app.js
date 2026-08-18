c150894d2a37b48d0c0ceae3544b675d08f5f45b
Artem-WebDeveloper <ave_art@icloud.com>
2026-08-18 14:52:40 +0300

    feat: add helmet for security headers

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/AppError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,15 +10,30 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-//! 1. MIDDLEWARES
-console.log(process.env.NODE_ENV);
+//! 1. GLOBAL MIDDLEWARES
+// Установка безопасности HTTP Headers
+app.use(helmet());
+
+// Логи в режиме разработки
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+// Ограничение количество запросов от одного и того IP
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please tru again in an hour',
+});
+app.use('/api', limiter);
+
+// Body парсер, читающий данные из body в body.req
+app.use(express.json({ limit: '10kb' }));
+
+// Обслуживание статических файлов
 app.use(express.static(`${__dirname}/public`));
 
+// Test Middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(req.headers);
